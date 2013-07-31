bd396cefc03be9d5c21fcab6748ceca5da3f9063
Jacek Radzikowski <jacek@dragon32.piranet.org>
2013-07-31 13:27:42 -0400

    Updated bbb_enable_gpio.sh

--- tools/bbb_enable_gpio.sh
+++ tools/bbb_enable_gpio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #workaround for a bug in bone-pinmux-helper to enable GPIO modules.
+#based on the code from Luigi Rinaldi: https://groups.google.com/forum/#!msg/beagleboard/OYFp4EXawiI/Mq6s3sg14HoJ
 
 EXPORT=/sys/class/gpio/export
 echo 5 > /sys/class/gpio/export
